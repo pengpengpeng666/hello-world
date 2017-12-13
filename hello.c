06d126a3b712f5133c5d4732cf33536095a88c36
lxp <lxp@lxp>
2017-12-13 06:37:04 +0000

    add sub process to print hello

--- hello.c
+++ hello.c
@@ -4,7 +4,13 @@ void main(void)
 {
     int i;
     printf("hello gitlab\n");
-    fork();
-    printf("hello fork\n");
-    scanf("%d", &i);
+    if(0 == fork()){
+        for(i=0; i<8; i++)
+            printf("hello fork\n");
+    }else{
+        usleep(16);
+        for(i=0; i<8; i++)
+            printf("hello world\n");
+    }
+    printf("Fine.\n");
 }
