8f503356104c088e1948779c0e74a194fcd70d4e
lxp <lxp@lxp>
2017-12-13 06:14:12 +0000

    add fork()

--- hello.c
+++ hello.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
+#include<unistd.h>
 void main(void)
 {
-    printf("hello world");
+    int i;
+    printf("hello gitlab\n");
+    fork();
+    printf("hello fork\n");
+    scanf("%d", &i);
 }
